350a6ca9227a630a55377c5b4a4b0b41761ae1e7
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-11-18 15:01:11 +0500

    sakizz

--- For08.py
+++ For08.py
@@ -6,10 +6,8 @@ def main(N):
     Returns:
         float: return  answer
     """
-    i = 1
-    a = 0
-    while i < N:
-        a = a + 1 / N
-    i += 1
-    return float(a)
-print(main(2))
+    s = 0
+    for i in range(1,N+1):
+        s+=1/i
+    return s
+print(main(4))
